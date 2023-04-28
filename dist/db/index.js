1782ae3414295f3855068981eb26514955802077
valyy151 <marin.valenta1@gmail.com>
2023-04-28 16:19:04 +0200

    Added start script for dist

--- dist/db/index.js
+++ dist/db/index.js
@@ -1,7 +1,11 @@
 "use strict";
-const mongoose = require('mongoose');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/server';
-mongoose
+mongoose_1.default
     .connect(MONGO_URI)
     .then(() => {
     console.log(`Connected to Database.`);
