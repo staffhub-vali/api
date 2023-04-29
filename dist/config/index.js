ddef4bea1c5f8d5d8adc48eb5dd9df23737bb391
valyy151 <marin.valenta1@gmail.com>
2023-04-29 20:32:51 +0200

    Ts node swc enabled

--- dist/config/index.js
+++ dist/config/index.js
@@ -7,7 +7,7 @@ const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000';
 module.exports = (app) => {
     app.set('trust proxy', 1);
     app.use(cors({
-        origin: [FRONTEND_URL, 'http://localhost:3000'],
+        origin: [FRONTEND_URL, 'http://localhost:3001'],
     }));
     app.use(logger('dev'));
     app.use(express.json());
