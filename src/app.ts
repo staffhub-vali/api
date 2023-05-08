dae9e8b1257eaa050b54508fe82f357b7b97d7a0
valyy151 <marin.valenta1@gmail.com>
2023-05-08 16:43:51 +0200

    Schedule details route

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/v1/auth', require('./routes/auth.routes'))
 app.use('/v1/roster', require('./routes/roster.routes'))
+app.use('/v1/schedules', require('./routes/schedule.routes'))
 app.use('/v1/employees', require('./routes/employees.routes'))
 
 app.listen(PORT, () => {
---
+++ src/routes/schedule.routes.ts
@@ -0,0 +1,27 @@
+import Schedule from '../models/Schedule.model'
+import express, { Request, Response } from 'express'
+import { Authenticate } from '../middleware/jwt.middleware'
+
+const router = express.Router()
+
+router.get('/', Authenticate, async (req, res) => {
+	try {
+		const schedules = await Schedule.find()
+		res.status(200).json(schedules)
+	} catch (error: any) {
+		console.log(error)
+		res.status(500).json({ message: error.message })
+	}
+})
+
+router.get('/:id', Authenticate, async (req, res) => {
+	try {
+		const schedule = await Schedule.findById(req.params.id)
+		res.status(200).json(schedule)
+	} catch (error: any) {
+		console.log(error)
+		res.status(500).json({ message: error.message })
+	}
+})
+
+module.exports = router
