e1b1acc953631575da397b31a4dda9572d500d3e
valyy151 <marin.valenta1@gmail.com>
2023-05-12 10:54:57 +0200

    First half of PUT route done for rosters

--- src/models/Shift.model.ts
+++ src/models/Shift.model.ts
@@ -3,8 +3,20 @@ import mongoose from 'mongoose'
 const shiftSchema = new mongoose.Schema({
 	end: { type: String },
 	start: { type: String },
-	employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' },
+	roster: { type: mongoose.Schema.Types.ObjectId, ref: 'Roster' },
 	workDay: { type: mongoose.Schema.Types.ObjectId, ref: 'WorkDay' },
+	employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' },
+})
+
+shiftSchema.pre('deleteMany', { document: true, query: false }, async function (next) {
+	const shift: { roster: string; workDay: string; _id: string } = this
+	try {
+		await mongoose.model('Roster').updateOne({ _id: shift.roster }, { $pull: { shifts: shift._id } })
+		await mongoose.model('WorkDay').updateOne({ _id: shift.workDay }, { $pull: { shifts: shift._id } })
+		next()
+	} catch (error: any) {
+		next(error)
+	}
 })
 
 const Shift = mongoose.model('Shift', shiftSchema)
